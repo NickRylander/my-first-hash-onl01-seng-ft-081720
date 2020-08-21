76b064cca0b5933297e4655c27487b968f9c26bc
Nicholas van Zuiden-Rylander <nick.rylander@outlook.com>
2020-08-21 03:02:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+   pets = {"dogs" => 5, "cats" => 7, "birds => 10}"
 end
 
 
